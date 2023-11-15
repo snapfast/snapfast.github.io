f0323f41da83558e29471defdb0979bfb1c7b46e
scor <rahulrdb18@gmail.com>
2023-11-15 23:58:11 +0530

    flasf

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+    trailingSlash: true,
    
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
