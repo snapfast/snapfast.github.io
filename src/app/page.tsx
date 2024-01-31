c6320ad166a3019b09242926c737980679940bf7
Rahul Bali <rahulrdb18@gmail.com>
2024-01-31 18:05:26 +0530

    fix the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,25 +29,29 @@ export default function Home() {
             @snapfast
           </a>
         </div>
-
+        {/* p tag is used for the new section */}
         <p>
-            Hello, my name is Rahul Bali, and I&apos;m a software engineer.
+          Hello, my name is Rahul Bali, and I&apos;m a software engineer.
         </p>
         <p>
           <br />
           <em>I recently started reading more about various different topics like music theory, how cosmos was born, Indian way of knowledge (shastra).</em>
           <em>I am happy to hear about your stories and things that you are learning. Lets talk from our hearts !</em>
           <br />
+        </p>
+        <p>
           <br />
           <em>This is the link to my Google calendar, please go ahead and setup a meet with video call.</em>
           <br />
-          <em><a href="https://calendar.google.com/calendar/u/0?cid=cmFodWxyZGIxOEBnbWFpbC5jb20" target="_blank" >https://calendar.google.com/calendar/u/0?cid=cmFodWxyZGIxOEBnbWFpbC5jb20</a></em>
-          <br />
-          <br />
         </p>
         <p>
-
-          DM on <a href="https://linkedin.com/in/rahulbali2" target="_blank" rel="noopener noreferrer" >LinkedIn</a> and <a href="https://instagram.com/bali.rahul" target="_blank" rel="noopener noreferrer" >Instagram</a>
+          <br />
+          <em><a href="https://calendar.google.com/calendar/u/0?cid=cmFodWxyZGIxOEBnbWFpbC5jb20" target="_blank" >https://calendar.google.com/calendar/u/0?cid=cmFodWxyZGIxOEBnbWFpbC5jb20</a></em>
+          <br />
+        </p>
+        <br />
+        <p>
+          DM on <a href="https://linkedin.com/in/rahulbali2" target="_blank" rel="noopener noreferrer" >LinkedIn</a>and<a href="https://instagram.com/bali.rahul" target="_blank" rel="noopener noreferrer" >Instagram</a>
         </p>
 
 
