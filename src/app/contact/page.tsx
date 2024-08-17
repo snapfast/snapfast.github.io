5991c15fd8b803c204c85fbce774c0ea9aea957b
Rahul Bali <rahulrdb18@gmail.com>
2024-08-17 19:45:59 +0530

    ok

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import styles from './../page.module.css'
 import Link from 'next/link'
 import Nav from '../nav'
-import Image from 'next/image';
 
 
 export default function Resume() {
